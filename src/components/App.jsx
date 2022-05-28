10b4f0f95fb6b9649c218a2de9d700f78bd74631
Iryna S <irynaradkovska@gmail.com>
2022-05-29 01:58:11 +0300

    adds home and search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Navigation from './Navigation';
+import Loader from './Loader';
+import HomePage from './HomePage';
+import MoviesPage from './MoviesPage';
+import NotFound from './NotFound';
+
+
+export function App() {
+  const [loading, setLoading] = useState(false);
+  const [films, setFilms] = useState([]);
+
+  const handleFilms = ({ films, loading }) => {
+    setFilms(films);
+    setLoading(loading);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage handleFilms={handleFilms} />}/>
+        <Route path='/movies' element={<MoviesPage handleFilms={handleFilms} />}/>
+        {/* <MovieDetailsPage /> */}
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      {loading && <Loader />}
     </div>
   );
 };
